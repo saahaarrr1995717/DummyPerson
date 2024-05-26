a17d323b3f8664b8e982897af471e1aa28a24860
ASUS <hoseinkhalafi74@gmail.com>
2024-05-26 20:38:13 +0430

    DummyPersonData

---
+++ DummyPersonData.java
@@ -0,0 +1,16 @@
+public class DummyPersonData {
+    public static void main(String[] args) {
+        byte age = 20;
+        short weight = 60;
+        int height = 160;
+        char bloodType = 'A';
+        long phoneNumber = 9121234567L;
+        String name = "Sahar";
+        Double cash = 100000000000D;
+        float cashTodollar = 16.6666F;
+        System.out.println("age: "+age+"\n"+"weight: "+weight+
+                "\n"+"height: "+height+"\n"+"blood type: "+bloodType+
+                "\n"+"phone number: "+phoneNumber+"\n"+"name: "+name+
+                "\n"+"cash: "+cash+"\n"+"cash to dollar: "+cashTodollar);
+    }
+}
